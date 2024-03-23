abfb4523308f512a499aeb870c607d9ba908ce00
Cal009 <153200653+Cal009@users.noreply.github.com>
2024-03-23 00:59:09 +0000

    Gameboard class made, print board function made too.

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+"""
+Creating the game board as the main area
+"""
+class GameBoard:
+    def __init__(self,board):
+        self.board = board
+
+    def print_board(self):
+        """
+        This creates the board itself, putting in spacers in the board grid
+        """
+        print("  1 2 3 4 5 6 7 8")
+        print("  ~~~~~~~~~~~~~~~")
+        row_number = 1
+        for row in self.board:
+            print("%d|%s|" % (row_number, "|".join(row)))
+            row_number += 1
