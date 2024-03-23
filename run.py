6780602f228b30fae9575293e632ec6f8460705e
Cal009 <153200653+Cal009@users.noreply.github.com>
2024-03-23 01:19:59 +0000

    create battleship function inside new battleship class

--- run.py
+++ run.py
@@ -16,3 +16,18 @@ class GameBoard:
         for row in self.board:
             print("%d|%s|" % (row_number, "|".join(row)))
             row_number += 1
+
+class Battleship:
+    def __init__(self, board):
+        self.board = board
+
+    def create_ships(self):
+        """
+        Function to create the ships
+        """
+        for i in range(5):
+            self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
+            while self.board[self.x_row][self.y_column] == "@":
+                self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
+            self.board[self.x_row][self.y_column] == "@"
+            return self.board
